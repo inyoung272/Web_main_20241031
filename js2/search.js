c51f457c25524a1ea15fd7b498da785ec04045fa
admin <inyoung276@gmail.com>
2025-04-02 13:47:45 +0900

    20241031_0402

--- js2/search.js
+++ js2/search.js
@@ -1,9 +1,17 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
+document.getElementById("search_btn").addEventListener('click', function() {
+    alert("검색을 수행합니다!");
+    googleSearch(); // 검색 메시지 띄운 후, 구글 검색 수행
+});
 
-function search_message(){}
 function googleSearch() {
     const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    if (!searchTerm) {
+        alert("검색어를 입력하세요!");
+        return;
+    }
+    
     const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
-    // 새 창에서 구글 검색을 수행
-    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
-    return false;}
+    
+    // 새 창에서 구글 검색 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기
+}
