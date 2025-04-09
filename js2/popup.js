8aea575eeeead98d60fce7ee13e570f654eddc07
admin <inyoung276@gmail.com>
2025-04-09 13:42:01 +0900

    20250409

--- js2/popup.js
+++ js2/popup.js
@@ -6,20 +6,29 @@ function show_clock(){
     let currentDate= new Date(); // 현재시스템날짜객체생성
    let divClock= document.getElementById('divClock');
     let msg = "현재시간: ";
+    
     if(currentDate.getHours()>12){  // 12시보다크면오후아니면오전
-   msg += "오후";
-    msg += currentDate.getHours()-12+"시";
+        msg += "오후";
+        msg += currentDate.getHours()-12+"시";
     }
     else {
-    msg += "오전";
-    msg += currentDate.getHours()+"시";
+        msg += "오전";
+        msg += currentDate.getHours()+"시";
     }
+    
     msg += currentDate.getMinutes()+"분";
-    msg += currentDate.getSeconds()+"초";
-    divClock.innerText= msg;
-    if (currentDate.getMinutes()>58) {    //정각1분전빨강색출력
-   divClock.style.color="red";
+        msg += currentDate.getSeconds()+"초";
+        divClock.innerText= msg;
+    
+        if (currentDate.getMinutes()>58) {    //정각1분전빨강색출력
+        divClock.style.color="red";
     }
     setTimeout(show_clock, 1000);  //1초마다갱신
    }
    
+   function over(obj) {
+    obj.src="../image/logo_steam.svg";
+    }
+    function out(obj) {
+    obj.src="image/steam_blue.png";
+    }
